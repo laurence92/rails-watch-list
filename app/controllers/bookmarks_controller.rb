e9d0bc24697a7f03dd4cf845e8e5ed328837804e
Laurence Sainsbury <laurencesainsbury@gmail.com>
2021-05-27 10:24:18 +0100

    back end complete

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,16 +6,21 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    if @bookmark.save
-      redirect_to list_path
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to list_path(@list)
     else 
       render :new
     end
   end
 
   def destroy
-    
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
   end
   
   private
